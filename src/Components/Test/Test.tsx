20e12c69e474e25c158d23a8f9d95b3914007c1d
Frederico <black_trim@hotmail.com>
2021-04-19 22:30:34 +0000

    test component

---
+++ src/Components/Test/Test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// Components
+// Context
+// Hooks
+// Pages
+// Resources
+
+export interface Props {}
+
+const Test: React.FC<Props> = ({}) => {
+    return <div>Test Component</div>;
+};
+
+export default Test;
