cb8d9aef35e5f7495c573ee38aa040ab2a9838b3
francisbm <francisco.bett.mendonca@gmail.com>
2021-04-21 16:46:58 +0000

    SearchBarc Component 2

---
+++ src/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+// Components
+// Context
+// Hooks
+// Pages
+// Resources
+
+const SearchBar: FC = () => {
+
+    return (
+        <div>
+            SearchBar Component 2
+        </div>
+    );
+};
+
+export default SearchBar;
