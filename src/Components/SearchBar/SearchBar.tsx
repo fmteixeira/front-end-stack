2ae422fee1f9003d41f517d653c6045119da3f7b
20182443Tiago <20182443@uac.pt>
2021-04-22 15:04:56 +0000

    SearchBar Component

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 // Components
 // Context
+// Media
+import search from "../../resources/media/icons/search.svg";
 // Hooks
 // Pages
 // Resources
@@ -8,7 +10,17 @@ import { FC } from "react";
 export interface Props {}
 
 const SearchBar: FC<Props> = ({}) => {
-    return <div>SearchBar Component</div>;
+    return (
+        <div className="bg-white-100 w-144 h-20 rounded-lg shadow-lg grid grid-cols-7 items-center">
+            <img className="ml-11 w-7" src={search} alt=""></img>
+            <input
+                className="col-span-5 w-45 h-6 mr-10 text-xl text-gray font-medium placeholder-gray focus:outline-none "
+                placeholder="Search"
+                type="text"
+                name="search"
+            />
+        </div>
+    );
 };
 
 export default SearchBar;
