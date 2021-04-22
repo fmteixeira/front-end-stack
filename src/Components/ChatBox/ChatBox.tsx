5ee0361ca23a207da58ad58339a41cba4d732319
francisbm <francisco.bett.mendonca@gmail.com>
2021-04-22 16:38:12 +0000

    Changes ChatBox

--- src/Components/ChatBox/ChatBox.tsx
+++ src/Components/ChatBox/ChatBox.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import clsx from "clsx";
 // Components
 // Media
-import search from "../../resources/media/icons/search.svg";
 // Context
 // Hooks
 // Pages
@@ -18,38 +17,30 @@ export interface Props {
 }
 
 const ChatBox: FC<Props> = ({ userAvatar, userName, msgText, lastMsgTime, msgsNum, active }) => {
-    // FIXME: Center msgNum text on circle (l.53) +9?
-    // TODO: Dynamic time ago
-    // FIXME: text-[13px]
     return (
         <div
             className={clsx(
                 active ? " bg-gradient-to-r from-blue-start to-blue-end " : "bg-white-100",
-                "w-[580px] h-[220px]",
-                "grid grid-cols-[54px,auto,auto] gap-4 p-7 rounded-md",
+                "w-auto h-auto",
+                "grid grid-cols-[auto,1fr,auto] gap-4 p-7 rounded-md",
             )}
         >
-            <img src={userAvatar} alt="" className="rounded-full h-[54px] w-[54px]" />
-
-            <div className="font-bold text-lg text-black">{userName}</div>
-
-            <div className={clsx(active ? "text-white-100 opacity-75" : "text-gray", "justify-self-end")}>
-                {lastMsgTime} minutes ago
+            <div>
+                <img src={userAvatar} alt="" className="rounded-full h-[54px] w-[54px]" />
             </div>
 
-            <div className="col-span-full overflow-hidden grid grid-cols-[auto,25px] gap-x-5">
-                <div
-                    className={clsx(
-                        active ? "text-white-100" : "text-gray",
-                        "col-start-1 col-end-2 text-gray",
-                        "text-justify",
-                    )}
-                >
-                    {msgText}
-                </div>
+            <span className="font-bold text-lg text-black">{userName}</span>
+
+            <span className={clsx(active ? "text-white-100 opacity-75" : "text-gray", "justify-self-end")}>
+                {lastMsgTime} minutes ago
+            </span>
+
+            <div className="col-span-full grid grid-cols-[1fr,auto] gap-x-5">
+                <p className={clsx(active ? "text-white-100" : "text-gray", "text-justify line-clamp-3")}>{msgText}</p>
+
                 {msgsNum > 0 && !active && (
-                    <div className="bg-pink rounded-full h-[20px] w-[20px] grid">
-                        <div className="text-white-100 text-[13px] justify-self-center self-center">{msgsNum}</div>
+                    <div className="bg-pink rounded-full h-[20px] w-[20px] grid place-items-center">
+                        <span className="text-white-100 text-[13px]">{msgsNum}</span>
                     </div>
                 )}
             </div>
