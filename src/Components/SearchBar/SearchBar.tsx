9a550e23bf65f15b1880d87297e69d984392c679
Talion1999 <69521585+Talion1999@users.noreply.github.com>
2021-04-22 15:41:49 +0000

    Update src/Components/SearchBar/SearchBar.tsx
    Co-authored-by: Frederico Teixeira <32296976+fmteixeira@users.noreply.github.com>

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -11,10 +11,10 @@ export interface Props {}
 
 const SearchBar: FC<Props> = ({}) => {
     return (
-        <div className="bg-white-100 w-144 h-20 rounded-lg shadow-lg grid grid-cols-7 items-center">
+        <div className="bg-white-100 w-144 h-20 rounded-lg shadow-lg grid grid-cols-[auto,1fr] items-center">
             <img className="ml-11 w-7" src={search} alt=""></img>
             <input
-                className="col-span-5 w-45 h-6 mr-10 text-xl text-gray font-medium placeholder-gray focus:outline-none "
+                className="w-45 h-6 mr-10 text-xl text-gray font-medium placeholder-gray focus:outline-none "
                 placeholder="Search"
                 type="text"
                 name="search"
