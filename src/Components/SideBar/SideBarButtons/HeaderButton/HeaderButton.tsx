c226d2dd647c506e641fc5f5e1b9cc1fa6396e70
20182443Tiago <20182443@uac.pt>
2021-04-24 13:22:05 +0000

    SideBar Melhoria de Svg

--- src/Components/SideBar/SideBarButtons/HeaderButton/HeaderButton.tsx
+++ src/Components/SideBar/SideBarButtons/HeaderButton/HeaderButton.tsx
@@ -30,7 +30,7 @@ const HeaderButton: FC<Props> = ({ titulo, active, setNav, chave }: Props) => {
                     className={clsx(
                         active ? "bg-blue" : "bg-transparent",
                         " w-1 h-11 rounded-lg",
-                        "transition ease-out duration-500",
+                        "transition ease-in-out duration-500",
                     )}
                 ></div>
                 <div>
@@ -40,7 +40,7 @@ const HeaderButton: FC<Props> = ({ titulo, active, setNav, chave }: Props) => {
                     className={clsx(
                         active ? "text-blue" : "text-gray",
                         "font-bold",
-                        "transition ease-out duration-500",
+                        "transition ease-in-out duration-500",
                         "col-start-4",
                     )}
                 >
--- src/Components/SideBar/SideBarButtons/HeaderButton/SvgIcon/SvgIcon.tsx
+++ src/Components/SideBar/SideBarButtons/HeaderButton/SvgIcon/SvgIcon.tsx
@@ -1,42 +1,17 @@
 import { FC } from "react";
-import clsx from "clsx";
 // Components
 // Context
 // Media
-import Calendar from "../../../../../resources/media/icons/sidebar/calendar.svg";
-import Chat from "../../../../../resources/media/icons/sidebar/chat.svg";
-import Home from "../../../../../resources/media/icons/sidebar/home.svg";
-import Notification from "../../../../../resources/media/icons/sidebar/notification.svg";
-import Setting from "../../../../../resources/media/icons/sidebar/setting.svg";
-import Contact from "../../../../../resources/media/icons/sidebar/contact.svg";
-import Logout from "../../../../../resources/media/icons/sidebar/logout.svg";
-import Calendar_Active from "../../../../../resources/media/icons/sidebar/calendar_active.svg";
-import Chat_Active from "../../../../../resources/media/icons/sidebar/chat_active.svg";
-import Home_Active from "../../../../../resources/media/icons/sidebar/home_active.svg";
-import Notification_Active from "../../../../../resources/media/icons/sidebar/notification_active.svg";
-import Setting_Active from "../../../../../resources/media/icons/sidebar/setting_active.svg";
-import Contact_Active from "../../../../../resources/media/icons/sidebar/contact_active.svg";
-import Logout_Active from "../../../../../resources/media/icons/sidebar/logout_active.svg";
+import { ReactComponent as CalendarSVG } from "../../../../../resources/media/icons/sidebar/calendar.svg";
+import { ReactComponent as ChatSVG } from "../../../../../resources/media/icons/sidebar/chat.svg";
+import { ReactComponent as HomeSVG } from "../../../../../resources/media/icons/sidebar/home.svg";
+import { ReactComponent as NotificationSVG } from "../../../../../resources/media/icons/sidebar/notification.svg";
+import { ReactComponent as SettingSVG } from "../../../../../resources/media/icons/sidebar/setting.svg";
+import { ReactComponent as ContactSVG } from "../../../../../resources/media/icons/sidebar/contact.svg";
+import { ReactComponent as LogoutSVG } from "../../../../../resources/media/icons/sidebar/logout.svg";
 // Hooks
 // Pages
 // Resources
-
-const icons: { [index: number]: string } = {
-    0: Home,
-    1: Chat,
-    2: Contact,
-    3: Notification,
-    4: Calendar,
-    5: Setting,
-    6: Logout,
-    7: Home_Active,
-    8: Chat_Active,
-    9: Contact_Active,
-    10: Notification_Active,
-    11: Calendar_Active,
-    12: Setting_Active,
-    13: Logout_Active,
-};
 
 interface Props {
     index: number;
@@ -44,22 +19,17 @@ interface Props {
 }
 
 const SvgIcon: FC<Props> = ({ index, active }: Props) => {
-    console.log(Calendar);
-    return (
-        <div>
-            <img className="absolute" src={icons[index + 7]} alt="" />
-            <img
-                className={clsx(
-                    active ? "opacity-0" : "opacity-100",
-                    "transition-all duration-700",
-                    "ease-linear",
-                    "relative",
-                )}
-                src={icons[index]}
-                alt=""
-            />
-        </div>
-    );
+    const iconsSVG = [
+        <HomeSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+        <ChatSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+        <ContactSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+        <NotificationSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+        <CalendarSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+        <SettingSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+        <LogoutSVG fill={active ? "#2A8BF2" : "#707C97"} className="transition-all ease-in-out duration-1000" />,
+    ];
+
+    return <div>{iconsSVG[index]}</div>;
 };
 
 export default SvgIcon;
