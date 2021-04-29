f580119ee87de202e7c515e02ec6eb77d8b0b70a
20182443Tiago <20182443@uac.pt>
2021-04-29 22:55:03 +0000

    ChatHeader Component

---
+++ src/Components/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+// Components
+// Context
+// Hooks
+// Pages
+// Resources
+
+export interface Props {
+};
+
+const ChatHeader: FC<Props> = ({}) => {
+
+    return (
+        <div>
+            ChatHeader Component
+        </div>
+    );
+};
+
+export default ChatHeader;
