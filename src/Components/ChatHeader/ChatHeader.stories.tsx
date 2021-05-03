4644f929bbc1f313106172fbc435448589c2a48f
20182443Tiago <20182443@uac.pt>
2021-05-03 22:56:23 +0000

    ChatHeader Prettier Fix

--- src/Components/ChatHeader/ChatHeader.stories.tsx
+++ src/Components/ChatHeader/ChatHeader.stories.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { withDesign } from "storybook-addon-designs";
 // Media
-import chatHeaderAvatar from "../../resources/media/icons/chatHeaderAvatar.svg"
+import chatHeaderAvatar from "../../resources/media/icons/chatHeaderAvatar.svg";
 // Component
 import ChatHeader, { Props } from "./ChatHeader";
 
 export default {
-  title: "ChatHeader",
-  component: ChatHeader,
-  decorators: [withDesign],
-  argTypes: {
-  },
+    title: "ChatHeader",
+    component: ChatHeader,
+    decorators: [withDesign],
+    argTypes: {},
 } as Meta;
 
 // Component Story
@@ -19,17 +18,16 @@ const Template: Story<Props> = (args) => <ChatHeader {...args} />;
 
 // Component Props
 const props: Props = {
-  avatarIcon: chatHeaderAvatar,
-  nome: "Nika Jerrardo",
+    avatarIcon: chatHeaderAvatar,
+    nome: "Nika Jerrardo",
 };
 
 // Component Story Node
 export const Default = Template.bind({});
 Default.args = props;
 Default.parameters = {
-  design: {
-    type: "figma",
-    url:
-      "https://www.figma.com/file/M0H6CQoJPQ6yNYy5q5jauC/Chat-Dashboard-(1)?node-id=616%3A651", // Figma Url
-  },
+    design: {
+        type: "figma",
+        url: "https://www.figma.com/file/M0H6CQoJPQ6yNYy5q5jauC/Chat-Dashboard-(1)?node-id=616%3A651", // Figma Url
+    },
 };
--- src/Components/ChatHeader/ChatHeader.tsx
+++ src/Components/ChatHeader/ChatHeader.tsx
@@ -2,23 +2,22 @@ import { FC } from "react";
 // Components
 // Context
 // Media
-import pin from "../../resources/media/icons/pin.svg"
-import verticalDots from "../../resources/media/icons/verticalDots.svg"
+import pin from "../../resources/media/icons/pin.svg";
+import verticalDots from "../../resources/media/icons/verticalDots.svg";
 // Hooks
 // Pages
 // Resources
 
 export interface Props {
-    avatarIcon: string,
-    nome: string
-};
+    avatarIcon: string;
+    nome: string;
+}
 
-const ChatHeader: FC<Props> = ({avatarIcon, nome}: Props) => {
-
+const ChatHeader: FC<Props> = ({ avatarIcon, nome }: Props) => {
     return (
         <div className="bg-white-100 rounded-t-md w-full h-24 grid grid-cols-[auto,1fr,auto,auto] gap-x-4 pl-4 pr-4 sm:pl-12 sm:pr-8 items-center">
             <div className="ml-auto w-10 h-10">
-                <img className="rounded-full" alt="" src={avatarIcon}/>
+                <img className="rounded-full" alt="" src={avatarIcon} />
             </div>
             <div>
                 <h2 className="font-bold text-sm sm:text-base">{nome}</h2>
@@ -26,12 +25,12 @@ const ChatHeader: FC<Props> = ({avatarIcon, nome}: Props) => {
             </div>
             <div>
                 <button className="bg-white-100 w-8 h-8 sm:w-11 sm:h-11 rounded-full shadow-buttonShadow grid items-center justify-items-center focus:outline-none">
-                    <img className="transform scale-75 sm:scale-100" alt="" src={pin}/>
+                    <img className="transform scale-75 sm:scale-100" alt="" src={pin} />
                 </button>
             </div>
             <div>
                 <button className="bg-white-100 w-8 h-8 sm:w-11 sm:h-11 rounded-full shadow-buttonShadow grid items-center justify-items-center focus:outline-none">
-                    <img className="transform scale-75 sm:scale-100" alt="" src={verticalDots}/>
+                    <img className="transform scale-75 sm:scale-100" alt="" src={verticalDots} />
                 </button>
             </div>
         </div>
