2d421ef29d89dda83b1099066ec56662e3d586b9
20182443Tiago <20182443@uac.pt>
2021-05-04 14:57:48 +0000

    DayLine Remoção de Linha Desnecessária

--- src/Components/DayLine/DayLine.tsx
+++ src/Components/DayLine/DayLine.tsx
@@ -14,7 +14,6 @@ const DayLine: FC<Props> = ({ date }: Props) => {
         return Math.floor((new Date().getTime() - date.getTime()) / (1000 * 3600 * 24));
     };
 
-    dataDay();
     return (
         <div className="grid grid-cols-[1fr,auto,1fr] items-center gap-x-3 sm:gap-x-6">
             <div className="bg-gray h-0.5 rounded-full opacity-50"></div>
