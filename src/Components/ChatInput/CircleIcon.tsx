9c5c2c600f5d1cd206c507ef8f6c536b97ae80d1
francisbm <francisco.bett.mendonca@gmail.com>
2021-05-06 22:19:15 +0000

    run prettier

--- src/Components/ChatInput/CircleIcon.tsx
+++ src/Components/ChatInput/CircleIcon.tsx
@@ -12,21 +12,24 @@ export interface Props {
     gradient: boolean;
 }
 
-const CircleIcon: FC<Props> = ({ icon, gradient}) => {
-
-    const [active, setActive] = useState(false)
+const CircleIcon: FC<Props> = ({ icon, gradient }) => {
+    const [active, setActive] = useState(false);
 
     return (
-        <div onMouseEnter={() => setActive(true)} onMouseLeave={()=> setActive(false)}
+        <div
+            onMouseEnter={() => setActive(true)}
+            onMouseLeave={() => setActive(false)}
             className={clsx(
                 "h-10 w-10 rounded-full grid place-items-center ",
                 active
-                    ? "bg-white-100 border-blue border-solid border-2" 
-                    : gradient ? "bg-gradient-to-r from-blue-start to-blue-end" : "bg-blue"
+                    ? "bg-white-100 border-blue border-solid border-2"
+                    : gradient
+                    ? "bg-gradient-to-r from-blue-start to-blue-end"
+                    : "bg-blue",
             )}
         >
-             <div>
-                 <img className={clsx(active ? "": "filter brightness-0 invert")} src={icon} alt=""></img>
+            <div>
+                <img className={clsx(active ? "" : "filter brightness-0 invert")} src={icon} alt=""></img>
             </div>
         </div>
     );
