07f837f89d8c902fe784791e9856e26f7f7baf6b
francisbm <francisco.bett.mendonca@gmail.com>
2021-05-21 19:09:55 +0000

    quickfix Message group stories props

--- src/Components/MessageGroup/MessageGroup.stories.tsx
+++ src/Components/MessageGroup/MessageGroup.stories.tsx
@@ -27,14 +27,14 @@ const props: Props = {
     userAvatar: userAvatar,
     messages: [
         {
-            id: 1,
+            id: "1",
             text:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, velit. Hic veniam tempore ullam debitis repellendus! Tempore veniam eius, recusandae doloremque at ex ullam rem, consequatur quidem cum perferendis eos.",
             date: new Date(2000, 4, 20, 12, 30),
             userId: 0,
         },
         {
-            id: 2,
+            id: "2",
             text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, debitis.",
             date: new Date(2000, 4, 20, 11, 30),
             file: {
@@ -46,7 +46,7 @@ const props: Props = {
             userId: 0,
         },
         {
-            id: 3,
+            id: "3",
             date: new Date(2000, 4, 20, 12, 30),
             file: {
                 fileName: "imagem1.jpg",
@@ -57,7 +57,7 @@ const props: Props = {
             userId: 1,
         },
         {
-            id: 4,
+            id: "4",
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, commodi.",
             date: new Date(2021, 4, 5, 13, 30),
             userId: 0,
