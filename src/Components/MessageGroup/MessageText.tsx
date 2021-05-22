b7eb5cdfcfdf60afa56fbc13bee21e6e8429d64a
francisbm <francisco.bett.mendonca@gmail.com>
2021-05-22 18:31:28 +0000

    fix posição tempo mensagem

--- src/Components/MessageGroup/MessageText.tsx
+++ src/Components/MessageGroup/MessageText.tsx
@@ -18,30 +18,33 @@ interface Props {
 
 const MessageText: FC<Props> = ({ text, date, isLast, isActiveUser }) => {
     return (
-        <div
-            className={clsx(
-                "grid gap-1 gap-x-3 items-center",
-                isActiveUser ? "grid-cols-[auto,1fr] " : "grid-cols-[1fr,auto]",
-            )}
-        >
-            <p
+        <div className={clsx("grid", isActiveUser ? "justify-items-end" : "justify-items-start")}>
+            <div
                 className={clsx(
-                    "p-3 rounded-xl shadow-lg",
-                    isActiveUser
-                        ? "rounded-br-none bg-white-100 text-gray text-right order-2 border-solid border-2 border-gray border-opacity-25"
-                        : "rounded-tl-none bg-gradient-to-r from-blue-start to-blue-end text-white-100",
+                    "grid gap-1 gap-x-3 items-center",
+                    isActiveUser ? "grid-cols-[auto,1fr]" : "grid-cols-[1fr,auto]",
                 )}
             >
-                {text}
-            </p>
+                <p
+                    className={clsx(
+                        "p-3 rounded-xl shadow-lg",
+                        isActiveUser
+                            ? "rounded-br-none bg-white-100 text-gray text-right order-2 border-solid border-2 border-gray border-opacity-25"
+                            : "rounded-tl-none bg-gradient-to-r from-blue-start to-blue-end text-white-100",
+                    )}
+                >
+                    {text}
+                </p>
 
-            <button className={clsx(isActiveUser && "order-1")}>
-                <img src={ellipsis} alt="" />
-            </button>
+                <button className={clsx(isActiveUser && "order-1")}>
+                    <img src={ellipsis} alt="" />
+                </button>
+            </div>
 
-            <div className={clsx(isActiveUser ? "order-3 col-start-2 justify-self-start" : "justify-self-end")}>
+            <div className={clsx(isActiveUser ? "ml-7 justify-self-start" : "justify-self-end mr-7")}>
                 {isLast && <MessageLastDate date={date}> </MessageLastDate>}
             </div>
+        
         </div>
     );
 };
